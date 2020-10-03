55dc2bc1c789c5467b1469ef214eac0b67620849
Lorenzo Milesi <lorenzo.milesi@live.fr>
2020-10-03 17:31:53 +0200

    refactor: roman numbers up to 3 Even better, replaced all StringBuilder logic by String.repeat
    Signed-off-by: Lorenzo Milesi <lorenzo.milesi@live.fr>

--- src/nbr/roman/Roman.java
+++ src/nbr/roman/Roman.java
@@ -6,8 +6,6 @@ public class Roman {
      * Converts int into Roman letters upto 3000.
      */
     public static String convert(int number) {
-        StringBuilder value = new StringBuilder();
-        value.append("I".repeat(number));
-        return value.toString();
+        return "I".repeat(number);
     }
 }
