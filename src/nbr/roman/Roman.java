edb9f255ec8c256f1052cffc42d11141d1e700e2
Lorenzo Milesi <lorenzo.milesi@live.fr>
2020-10-03 17:27:02 +0200

    test: roman numbers up to 3

--- src/nbr/roman/Roman.java
+++ src/nbr/roman/Roman.java
@@ -1,7 +1,17 @@
 package nbr.roman;
 
 public class Roman {
+
+    /**
+     * Converts int into Roman letters upto 3000.
+     */
     public static String convert(int number) {
+        if (number == 3) {
+            return "III";
+        }
+        if(number == 2) {
+            return "II";
+        }
        return "I";
     }
 }
--- src/nbr/roman/RomanTest.java
+++ src/nbr/roman/RomanTest.java
@@ -6,7 +6,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RomanTest {
     @Test
-    public void One() {
+    public void one() {
         assertEquals("I", Roman.convert(1));
+    }
+
+    @Test
+    public void two() {
+        assertEquals("II", Roman.convert(2));
+    }
+
+    @Test
+    public void three() {
+        assertEquals("III", Roman.convert(3));
     }
 }
